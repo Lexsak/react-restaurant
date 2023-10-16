66a4adb3c59258a6a50f716a5498be6ab6dbb683
Lexsak <bartoszpiprek123@gmail.com>
2023-10-16 14:38:50 +0200

    Footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="app__footer section__padding" id="login">
+      <FooterOverlay />
+      <Newsletter />
+
+      <div className="app__footer-links">
+        <div className="app__footer-links_contact">
+          <h1 className='app__footer-headtext'>Contact</h1>
+          <p className='p__opensans'> 23 W 44rd St, New York, NY 53223, USA</p>
+          <p className='p__opensans'> +1 111-222-333</p>
+          <p className='p__opensans'> +1 777-888-999</p>
+        </div>
+        <div className="app__footer-links_logo">
+          <img src={images.gericht} alt="gericht logo" />
+          <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others"</p>
+          <img src={images.spoon} alt="spoon" className='spoon__impg' style={{marginTop: '15px'}}/>
+          <div className="app__footer-links_icons">
+            <FiFacebook />
+            <FiTwitter />
+            <FiInstagram />
+          </div>
+        </div>
+        <div className="app__footer-links_work">
+          <h1 className='app__footer-headtext'>Working Hours</h1>
+          <p className='p__opensans'> Monday-Friday:</p>
+          <p className='p__opensans'> 08:00 am - 12:00 am</p>
+          <p className='p__opensans'> Saturday-Sunday:</p>
+          <p className='p__opensans'> 07:00 am - 11:00 pm</p>
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <p className="p__opensans">{currentYear} Gericht. All Rights Reserved.</p>
+      </div>
+    </div>
+
+  )
+};
 
 export default Footer;
